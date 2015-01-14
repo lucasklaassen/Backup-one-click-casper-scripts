f07ffc97ea70368a1c2067604cf1c38cf237465b
Lucas Klaassen <lucas@vin65.com>
2015-01-13 17:25:50 -0800

    Website Settings functions completed. Test quick settings for end timeout to be long enough

--- templates/designerLaunch/init.js
+++ templates/designerLaunch/init.js
@@ -29,11 +29,13 @@ if(casper.cli.args.length < 4) {
   var password = consoleRead("Enter password, then hit enter.");
   var userInputWebsiteName = consoleRead("Enter Website Name, then hit enter.");
   var userInputWebsiteURL = consoleRead("Enter the Website URL, then hit enter.");
+  var userInputWebsiteLocation = consoleRead("Enter the winery's location, then hit enter.");
 } else {
   var username = casper.cli.get(0);
   var password = casper.cli.get(1);
   var userInputWebsiteName = casper.cli.get(2);
   var userInputWebsiteURL = casper.cli.get(3);
+  var userInputWebsiteLocation = casper.cli.get(4);
 }
 
 casper.start();
@@ -51,107 +53,8 @@ vin65.validateWebsiteID();
 // vin65.initProductLayouts();
 //End Product Layout Defaults
 
-// //Website Settings Pages Array
-var websiteSettingsPagesArray = ['/settings/index.cfm?method=websiteSettings.EditComponents',
-'/settings/index.cfm?method=websiteSettings.CopyPages',
-'/settings/index.cfm?method=websiteSettings.Ecommerce',
-'/settings/index.cfm?method=websiteSettings.CopyWineAttributes',
-'/settings/index.cfm?method=websiteSettings.CopyBlogPosts',
-'/settings/index.cfm?method=websiteSettings.CopyEventCalendars',
-'/settings/index.cfm?method=websiteSettings.CopyShipping',
-'/settings/index.cfm?method=websiteSettings.editQuickSettings'];
-
-// //Website Settings Automation
-casper.wait(1000, function() {
-    this.evaluate(function() {
-      $('html').prepend('<iframe src="/settings/index.cfm?method=websiteSettings.loadSettings" name="websiteSettings" class="websiteSettings"></iframe');
-      $('html').prepend('<iframe name="websiteSettingsFunctions" class="websiteSettingsFunctions"></iframe>');
-      $('.websiteSettingsFunctions').css('height', '200px');
-      $('.websiteSettingsFunctions').css('width', '200px');
-    });
-  this.wait(1000, function(){
-    this.evaluate(function(websiteSettingsPagesArray) {
-      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[0]);
-    }, websiteSettingsPagesArray);
-  });
-  this.wait(1000, function(){
-    this.evaluate(function() {
-      $('.websiteSettingsFunctions').contents().find('input').prop('checked', true);
-    });
-    this.wait(1000, function() {
-      this.withFrame('websiteSettingsFunctions', function() {
-        this.click('#popupFooterRight > a > img');
-      });
-    });
-  });
-  this.wait(4000, function() {
-    this.evaluate(function(websiteSettingsPagesArray) {
-      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[1]);
-    }, websiteSettingsPagesArray);
-  });
-  this.wait(1000, function(){
-    var copyPages = this.evaluate(function() {
-      var copyPagesBoolean;
-      var setWebsiteID = function(id) {
-        $('.websiteSettingsFunctions').contents().find("select[name='fromWebsiteID']").val(id);
-      }
-      $('.websiteSettingsFunctions').contents().find("select[name='fromWebsiteID']").find('option').each(function(){
-        if($(this).text() === "Vin65 Designer Launch Template"){
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else if($(this).text() === "Vin65Cloud2 Pages Template") {
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else if($(this).text() === "Vin65cloud3 Pages Template") {
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else if($(this).text() === "Vin65Cloud4 Pages Template") {
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else if($(this).text() === "Vin65 Australia Template 1") {
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else if($(this).text() === "IBG Pages Template") {
-          setWebsiteID($(this).val());
-          copyPagesBoolean = true;
-          return false;
-        } else {
-          copyPagesBoolean = false;
-        }
-      });
-      return copyPagesBoolean;
-    });
-    this.wait(2000, function() {
-      if(copyPages === false) {
-        this.echo("There are no websites to copy pages from. :( ").exit();
-      }
-    });
-    this.wait(1000, function() {
-      this.withFrame('websiteSettingsFunctions', function() {
-        this.click('#popupFooterRight > a > img');
-      });
-    });
-    this.wait(4000, function() {
-      this.evaluate(function(websiteSettingsPagesArray) {
-        $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[2]);
-      }, websiteSettingsPagesArray);
-    });
-    this.wait(1000, function() {
-      this.evaluate(function() {
-        $('.websiteSettingsFunctions').contents().find('input').prop('checked', true);
-      });
-    });
-    this.wait(1000, function() {
-      this.withFrame('websiteSettingsFunctions', function() {
-        this.click('#popupFooterRight > a > img');
-      });
-    });
-  });
-});
+//Init Website Settings Functions
+vin65.websiteSettingsFunctions();
+//End Website Settings Functions
 
 casper.run();
--- templates/designerLaunch/vin65.js
+++ templates/designerLaunch/vin65.js
@@ -93,3 +93,214 @@ exports.initProductLayouts = function() {
 	});
 	oneClick.stageComplete('#productLayoutsComplete', 'Product Layouts were initialized!');
 }
+
+exports.websiteSettingsFunctions = function() {
+	//Website Settings Pages Array
+	var websiteSettingsPagesArray = ['/settings/index.cfm?method=websiteSettings.EditComponents',
+	'/settings/index.cfm?method=websiteSettings.CopyPages',
+	'/settings/index.cfm?method=websiteSettings.Ecommerce',
+	'/settings/index.cfm?method=websiteSettings.Clubs',
+	'/settings/index.cfm?method=websiteSettings.CopyWineAttributes',
+	'/settings/index.cfm?method=websiteSettings.CopyBlogPosts',
+	'/settings/index.cfm?method=websiteSettings.CopyEventCalendars',
+	'/settings/index.cfm?method=websiteSettings.CopyShipping',
+	'/settings/index.cfm?method=websiteSettings.editQuickSettings'];
+	var initCheckboxes = function() {
+	  casper.evaluate(function() {
+	    $('.websiteSettingsFunctions').contents().find('input').prop('checked', true);
+	  });
+	}
+	var initSelectOptions = function() {
+	  casper.evaluate(function() {
+	    var copyPagesBoolean;
+	    var setWebsiteID = function(id) {
+	      $('.websiteSettingsFunctions').contents().find("select[name='fromWebsiteID']").val(id);
+	    }
+	    $('.websiteSettingsFunctions').contents().find("select[name='fromWebsiteID']").find('option').each(function(){
+	      if($(this).text() === "Vin65 Designer Launch Template"){
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else if($(this).text() === "Vin65Cloud2 Pages Template") {
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else if($(this).text() === "Vin65cloud3 Pages Template") {
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else if($(this).text() === "Vin65Cloud4 Pages Template") {
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else if($(this).text() === "Vin65 Australia Template 1") {
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else if($(this).text() === "IBG Pages Template") {
+	        setWebsiteID($(this).val());
+	        copyPagesBoolean = true;
+	        return false;
+	      } else {
+	        copyPagesBoolean = false;
+	      }
+	    });
+	    return copyPagesBoolean;
+	  });
+	}
+	var initSubmit = function() {
+	  casper.withFrame('websiteSettingsFunctions', function() {
+	    this.click('#popupFooterRight > a > img');
+	  });
+	}
+	//Website Settings Automation
+	casper.wait(1000, function() {
+	    this.evaluate(function() {
+	      $('html').prepend('<iframe src="/settings/index.cfm?method=websiteSettings.loadSettings" name="websiteSettings" class="websiteSettings"></iframe');
+	      $('html').prepend('<iframe name="websiteSettingsFunctions" class="websiteSettingsFunctions"></iframe>');
+	      $('.websiteSettingsFunctions').css('height', '200px');
+	      $('.websiteSettingsFunctions').css('width', '200px');
+	    });
+	  this.wait(1000, function(){
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[0]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function(){
+	    initCheckboxes();
+	    this.wait(1000, function() {
+	      initSubmit();
+	    });
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[1]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function(){
+	    var copyPages = initSelectOptions();
+	    this.wait(2000, function() {
+	      if(copyPages === false) {
+	        this.echo("There are no websites to copy pages from. :( ").exit();
+	      }
+	    });
+	    this.wait(1000, function() {
+	      initSubmit();
+	    });
+	    this.wait(4000, function() {
+	      this.evaluate(function(websiteSettingsPagesArray) {
+	        $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[2]);
+	      }, websiteSettingsPagesArray);
+	    });
+	    this.wait(1000, function() {
+	      initCheckboxes();
+	    });
+	    this.wait(1000, function() {
+	      initSubmit();
+	    });
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[3]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function() {
+	    initCheckboxes();
+	  });
+	  this.wait(1000, function() {
+	    initSubmit();
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[4]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function() {
+	    var copyPages = initSelectOptions();
+	    this.wait(2000, function() {
+	      if(copyPages === false) {
+	        this.echo("There are no websites to copy pages from. :( ").exit();
+	      }
+	    });
+	  });
+	  this.wait(1000, function() {
+	    initCheckboxes();
+	  });
+	  this.wait(1000, function() {
+	    initSubmit();
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[5]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function() {
+	    var copyPages = initSelectOptions();
+	    this.wait(2000, function() {
+	      if(copyPages === false) {
+	        this.echo("There are no websites to copy pages from. :( ").exit();
+	      }
+	    });
+	  });
+	  this.wait(1000, function() {
+	    initSubmit();
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[6]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function() {
+	    var copyPages = initSelectOptions();
+	    this.wait(2000, function() {
+	      if(copyPages === false) {
+	        this.echo("There are no websites to copy pages from. :( ").exit();
+	      }
+	    });
+	  });
+	  this.wait(1000, function() {
+	    initSubmit();
+	  });
+	  this.wait(4000, function() {
+	    this.evaluate(function(websiteSettingsPagesArray) {
+	      $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[7]);
+	    }, websiteSettingsPagesArray);
+	  });
+	  this.wait(1000, function() {
+	    var copyPages = initSelectOptions();
+	    this.wait(2000, function() {
+	      if(copyPages === false) {
+	        this.echo("There are no websites to copy pages from. :( ").exit();
+	      }
+	    });
+	  });
+	  this.wait(1000, function() {
+	    initSubmit();
+	  });
+	  // Quick Settings
+	  this.then(function() {
+      this.wait(4000, function(){
+        this.evaluate(function(websiteSettingsPagesArray) {
+          $('.websiteSettingsFunctions').attr("src", websiteSettingsPagesArray[8]);
+        }, websiteSettingsPagesArray);
+      });
+      this.wait(1000, function() {
+        this.evaluate(function(userInputWebsiteLocation) {
+          $('.websiteSettingsFunctions').contents().find('[name="countryCode"]').val(userInputWebsiteLocation);
+        }, userInputWebsiteLocation);
+        initSelectOptions();
+      });
+      this.wait(1000, function() {
+      	initSubmit();
+      });
+	  });
+		this.then(function() {
+			this.wait(10000, function() {
+				this.evaluate(function() {
+				  $('html').prepend('<div id="websiteFunctionsComplete"></div>');
+				});
+			});
+		});
+	});
+oneClick.stageComplete('#websiteFunctionsComplete', 'Website Functions were run successfully!');
+}
